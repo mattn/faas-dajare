8abb1322c08099e439e22302a94a268ddd59ed54
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-05-15 11:54:49 +0900

    first import

---
+++ dajare/handler.go
@@ -0,0 +1,42 @@
+package function
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	db *sql.DB
+)
+
+func init() {
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	db, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	var text string
+	err := db.QueryRow(`SELECT text FROM dajare ORDER BY RANDOM() LIMIT 1`).Scan(&text)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(struct {
+		Text string `json:"text"`
+	}{
+		Text: text,
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
